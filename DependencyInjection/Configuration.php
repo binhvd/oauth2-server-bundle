a4b91157cb250a047017b241914e47565c44aed0
Binh Vu <bvu@ftk.de>
2021-07-13 11:44:29 +0200

    patch for Symfony 4 and older send new refresh_token in response for refresh_token request

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,13 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('oauth2_server');
+        $treeBuilder = new TreeBuilder('oauth2_server');
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('oauth2_server');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
